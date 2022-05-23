232094162305de5332970508c7ba513118e7490f
Shawn Moses <shawnmoses1847@gmail.com>
2022-05-23 22:47:18 +0530

    Codeforces Grind lmao

--- Current/main.cpp
+++ Current/main.cpp
@@ -1,75 +1,58 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define int long long int
 
-int n, m;
+int n;
+bool possible = true;
 
-void update(vector<int>& tree, int i, int delta) {
-    while (i <= n) {
-        tree[i] += delta;
-        i += i & -i;
-    }
-}
-
-int sum(vector<int>& tree, int i) {
-    int ret = 0;
-    while (i) {
-        ret += tree[i];
-        i -= i & -i;
-    }
-
-    return ret;
-}
-
-void solve() {
-    cin >> n >> m;
-    vector<int> rows(n + 1);
-    vector<int> cols(n + 1);
-
-    vector<int> fenwick_rows, fenwick_cols;
-
-    fenwick_rows.assign(n + 1, 0);
-    fenwick_cols.assign(n + 1, 0);
-
-    while (m--) {
-        int query;
-        cin >> query;
-        if (query == 1) {
-            int x, y;
-            cin >> x >> y;
-            if (++rows[x] == 1) update(fenwick_rows, x, 1);
-            if (++cols[y] == 1) update(fenwick_cols, y, 1);
-        } else if (query == 2) {
-            int x, y;
-            cin >> x >> y;
-            if (--rows[x] == 0) update(fenwick_rows, x, -1);
-            if (--cols[y] == 0) update(fenwick_cols, y, -1);
-        } else {
-            //            {
-            //                for (int i = 1; i < n; i++) {
-            //                    cout << sum(fenwick_rows, i) -
-            //                    sum(fenwick_rows, i - 1);
-            //                }
-            //                cout << endl;
-            //                for (int i = 1; i < n; i++) {
-            //                    cout << sum(fenwick_cols, i) -
-            //                    sum(fenwick_cols, i - 1);
-            //                }
-            //            }
-            int x1, y1, x2, y2;
-            cin >> x1 >> y1 >> x2 >> y2;
-            int x_sum = sum(fenwick_rows, x2) - sum(fenwick_rows, x1 - 1);
-            int y_sum = sum(fenwick_cols, y2) - sum(fenwick_cols, y1 - 1);
-
-            if (x_sum == (x2 - x1 + 1) || y_sum == (y2 - y1 + 1))
-                cout << "Yes" << endl;
-            else
-                cout << "No" << endl;
+map<int, int> mp;
+int recur(int x) {
+    if (mp.count(x)) return mp[x];
+    string str = to_string(x);
+    if (str.length() == n) return 0;
+    if (str.length() > n) return -1;
+    vector<int> p;
+    for (auto s : str) {
+        if (s > '1') {
+            int sol = 1 + recur(x * (s - '0'));
+            if (sol != -1) p.push_back(sol);
         }
     }
+    if (p.size() == 0)
+        return -1;
+    else
+        return mp[x] = *(min_element(p.begin(), p.end()));
+}
+
+// void solve() {
+//     int n, x;
+//     cin >> n >> x;
+//
+//     int counter = 0;
+//     while (true) {
+//         string temp = to_string(x);
+//         int mx = *(max_element(temp.begin(), temp.end())) - '0';
+//         int newx = x * mx;
+//         counter++;
+//         if (newx == x) {
+//             cout << -1 << endl;
+//             return;
+//         }
+//         if (to_string(newx).length() == n) {
+//             break;
+//         }
+//         x = newx;
+//
+//     }
+//     cout << counter << endl;
+// }
+
+void solve() {
+    int x;
+    cin >> n >> x;
+    cout << recur(x) << endl;
 }
 
 int32_t main() {
